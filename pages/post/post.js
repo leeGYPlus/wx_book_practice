f0b8bf1a345a25e525c3828828f27f7679d5f544
liguoying <15510224541@163com>
2019-12-09 11:00:03 +0800

    修改状态栏的颜色

--- pages/post/post.js
+++ pages/post/post.js
@@ -1,10 +1,103 @@
 Page({
-    imgUrls:[
-        "/images/post/post-1@text.jpg",
-        "/images/post/post-2@text.jpg",
-        "/images/post/post-3@text.jpg",
-    ],
+    data: {
+        // postList: [
+        //     {
+        //         object: {
+        //             date: "19.12.04",
+        //         },
+        //         title: "我们的回忆",
+        //         postImg: "/images/post/post-4.jpg",
+        //         avatar: "/images/avatar/avatar-5.png",
+        //         content: "我们之间的回忆忽远又忽近。",
+        //         readingNum: 102,
+        //         collectionNum: {
+        //             array: [109]
+        //         },
+        //         commentNum: 8
+        //     },
+        //     {
+        //         object: {
+        //             date: "19.12.04",
+        //         },
+        //         title: "我们的回忆",
+        //         postImg: "/images/post/post-5.jpg",
+        //         avatar: "/images/avatar/avatar-1.png",
+        //         content: "我们之间的回忆忽远又忽近。",
+        //         readingNum: 102,
+        //         collectionNum: {
+        //             array: [109]
+        //         },
+        //         commentNum: 8
+        //     },
+        //     {
+        //         object: {
+        //             date: "19.12.04",
+        //         },
+        //         title: "我们的回忆",
+        //         postImg: "/images/post/post-1.jpg",
+        //         avatar: "/images/avatar/avatar-3.png",
+        //         content: "我们之间的回忆忽远又忽近。",
+        //         readingNum: 102,
+        //         collectionNum: {
+        //             array: [109]
+        //         },
+        //         commentNum: 8
+        //     },
+        // ]
+    },
+    onLoad: function () {
+        var postList = [
+            {
+                object: {
+                    date: "19.12.04",
+                },
+                title: "我们的回忆",
+                postImg: "/images/post/post-4.jpg",
+                avatar: "/images/avatar/avatar-5.png",
+                content: "我们之间的回忆忽远又忽近。",
+                readingNum: 102,
+                collectionNum: {
+                    array: [109]
+                },
+                commentNum: 8
+            },
+            {
+                object: {
+                    date: "19.12.04",
+                },
+                title: "我们的回忆",
+                postImg: "/images/post/post-5.jpg",
+                avatar: "/images/avatar/avatar-1.png",
+                content: "我们之间的回忆忽远又忽近。",
+                readingNum: 102,
+                collectionNum: {
+                    array: [109]
+                },
+                commentNum: 8
+            },
+            {
+                object: {
+                    date: "19.12.04",
+                },
+                title: "我们的回忆",
+                postImg: "/images/post/post-1.jpg",
+                avatar: "/images/avatar/avatar-3.png",
+                content: "我们之间的回忆忽远又忽近。",
+                readingNum: 102,
+                collectionNum: {
+                    array: [109]
+                },
+                commentNum: 8
+            },
+        ]
+        // this.setData 用来更新 data 中的数据
+        this.setData({
+            postList:postList
+        })
+    },
     bindchange(e){
         console.log(e)
     }
 })
+
+
